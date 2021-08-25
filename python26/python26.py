23c851345eab4ae687017558009676740081246d
naththanicha ardsuwan <naththanicha@gmail.com>
2021-08-25 16:12:07 +0700

    chapter2

--- python26/python26.py
+++ python26/python26.py
@@ -1,7 +1,10 @@
-def weirdSubtract(n,k):
-
-	### Enter Your Code Here ###
-
-n,s = input("Enter num and sub : ").split()
-
-print(weirdSubtract(int(n),int(s)))
+def weirdSubtract(num,sub):			
+	for i in range(sub) :
+		if num % 10 ==0 :
+			num //= 10 # หารเอาเลขลงตัวำม่เอาเศษ
+		else :
+			num -= 1
+	return num
+	
+num,sub = input("Enter num and sub : ").split()
+print(weirdSubtract(int(num),int(sub)))
