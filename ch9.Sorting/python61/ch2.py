6e8e57f4041f2259f95212777d1e9ac8df2f0348
Kukul <naththanicha@gmail.com>
2021-11-18 02:24:16 +0700

    up her

---
+++ ch9.Sorting/python61/ch2.py
@@ -0,0 +1,17 @@
+def selectSort(inp,n) :
+    if n == 0 :
+        return inp
+    else :
+        temp = max(inp[:n+1])
+        inp[inp.index(temp)] = inp[n]
+        a = inp[n]
+        if temp != inp[n] :
+            print('swap {} <-> {} : '.format(inp[n],temp),end='')
+        inp[n] = temp
+        if temp != a :
+            print(inp)
+    selectSort(inp,n-1)
+
+inp = list(map(int,input("Enter Input : ").split()))
+selectSort(inp,len(inp)-1)
+print(inp)
---
+++ ch9.Sorting/python62/ch3.py
@@ -0,0 +1,23 @@
+def insertSort(arr , n) :
+    if n <= 1 :
+        return
+    insertSort(arr,n-1)
+    last = arr[n-1]   
+    j = n - 2  
+    j = loopRecur(arr, j, last)     
+    arr[j+1] = last   
+    if len(arr) != n:
+        print("insert {} at index {} :".format(last,j+1), arr[:n], arr[n:])
+    else:
+        print("insert {} at index {} :".format(last,j+1), arr)
+
+def loopRecur (arr,j,last) :
+    if j < 0 or arr[j] < last :
+        return j 
+    arr[j+1] = arr[j]
+    return loopRecur(arr,j-1,last)
+
+inp = list(map(int,input("Enter Input : ").split()))
+insertSort(inp,len(inp))
+print("sorted")
+print(inp)
